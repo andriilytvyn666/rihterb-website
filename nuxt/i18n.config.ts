8b212354f5874b2d5b2a753ed896c86b3451a3fd
Andrii <lytvyn349@gmail.com>
2023-04-18 21:35:36 +0300

    build: upgrade i18n module

---
+++ nuxt/i18n.config.ts
@@ -0,0 +1,11 @@
+import en from './locales/en-US.json'
+import uk from './locales/uk-UA.json'
+import ru from './locales/ru-RU.json'
+
+export default defineI18nConfig((nuxt) => ({
+  legacy: false,
+  locale: 'uk',
+  fallbackLocale: 'en',
+  availableLocales: ['en', 'uk', 'ru'],
+  messages: { en, uk, ru },
+}))
