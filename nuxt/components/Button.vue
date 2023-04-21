6ab8b568b5b8c9b8004e4fa26a6438c9941e33f5
Andrii <lytvyn349@gmail.com>
2023-04-22 02:51:23 +0300

    feat: make journal viewer responsive

--- nuxt/components/Button.vue
+++ nuxt/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex items-center justify-center w-full gap-4 px-6 py-4 rounded-lg text-body-md-500 sm:w-fit flex-nowrap line-clamp-1"
+    class="min-h-[3.75rem] flex items-center justify-center w-full gap-4 px-6 py-4 rounded-lg text-body-md-500 sm:w-fit flex-nowrap line-clamp-1"
   >
     <slot />
     <span :class="`${name !== undefined ? 'truncate' : 'hidden'}`">{{
--- nuxt/pages/journal/index.vue
+++ nuxt/pages/journal/index.vue
@@ -1,12 +1,12 @@
 <template>
   <NuxtLayout name="nowrapper">
-    <JournalBg />
+    <JournalBg class="hidden sm:block" />
     <transition name="fade">
       <div
-        class="z-10 w-full grow h-full flex items-center justify-center overflow-visible"
+        class="z-10 w-full flex grow items-center justify-center overflow-visible"
         v-if="store.gatherPages"
       >
-        <div class="flex flex-col gap-5 overflow-visible items-center">
+        <div class="flex flex-col grow gap-5 overflow-visible items-center">
           <Swiper
             :modules="[SwiperNavigation]"
             :slides-per-view="1"
@@ -16,20 +16,25 @@
               prevEl: '#swiper-prev',
               disabledClass: 'opacity-0',
             }"
-            class="w-[59rem] items-center flex justify-center grow h-[45.625rem] overflow-visible bg-dark"
+            class="w-screen h-[36rem] sm:w-[59rem] items-center flex grow justify-center sm:h-[45.625rem] overflow-visible bg-dark"
           >
             <SwiperSlide
               v-for="i in 12"
               :key="i"
-              class="flex justify-center items-center grow overflow-visible relative"
+              class="px-4 sm:px-0 flex justify-center grow items-center overflow-visible"
             >
-              <NuxtImg :src="`journal_view/${i}.webp`" class="h-full" />
+              <NuxtImg
+                :src="`journal_view/${i}.webp`"
+                class="sm:h-full object-cover"
+              />
             </SwiperSlide>
           </Swiper>
-          <div class="flex grow gap-4 w-[35rem]">
+          <div
+            class="flex grow gap-4 px-4 sm:px-0 w-screen sm:w-[35rem] max-h-[3.75rem]"
+          >
             <button
               id="swiper-prev"
-              class="flex items-center justify-center border border-dark-border p-4 rounded-lg text-[1.5rem]"
+              class="min-w-[3.75rem] min-h-[3.75rem] flex items-center justify-center border border-dark-border p-4 rounded-lg text-[1.5rem]"
             >
               <NuxtIcon name="feather/arrow-left" filled class="w-6 h-6" />
             </button>
@@ -42,7 +47,7 @@
             <!-- </NuxtLink> -->
             <button
               id="swiper-next"
-              class="flex items-center justify-center border border-dark-border p-4 rounded-lg text-[1.5rem]"
+              class="min-w-[3.75rem] min-h-[3.75rem] flex items-center justify-center border border-dark-border p-4 rounded-lg text-[1.5rem]"
             >
               <NuxtIcon name="feather/arrow-right" filled class="w-6 h-6" />
             </button>
@@ -51,31 +56,39 @@
       </div>
     </transition>
     <transition name="fade">
-      <!-- <div class="h-full w-full grow flex items-center justify-center"> -->
-      <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div
+        class="h-full w-full grow flex items-center justify-center"
+        v-if="!store.gatherPages"
+      >
         <div
-          :class="`duration-600 bg-light shadow-default p-8 gap-8 flex flex-col items-center w-[40rem] rounded-lg`"
-          v-if="!store.gatherPages"
+          class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-auto px-4"
         >
-          <div class="gap-5 flex flex-col items-center">
-            <h2 class="text-h-lg-700 text-hl-blue">журнал “дев’ять”</h2>
-            <p class="text-body-md-500 text-dark">
-              У журналі "дев'ять" ви знайдете цікаві статті про новітні
-              тенденції у світі мистецтва та літератури та роздуми про глибинні
-              філософські питання.
-            </p>
-          </div>
-          <div class="flex gap-4">
-            <NuxtLink :to="localePath('/', $i18n.locale)">
-              <Button name="на головну" class="border text-dark border-dark" />
-            </NuxtLink>
-            <!-- <NuxtLink :to="localePath('/journal/view', $i18n.locale)"> -->
-            <Button
-              name="дивитись"
-              class="bg-dark text-light"
-              @click="store.toggleGatherPages"
-            />
-            <!-- </NuxtLink> -->
+          <div
+            :class="`duration-600 bg-light shadow-default p-8 gap-8 flex w-full flex-col items-center sm:w-[38rem] rounded-lg`"
+          >
+            <div class="gap-5 flex flex-col items-center">
+              <h2 class="text-h-lg-700 text-hl-blue">журнал “дев’ять”</h2>
+              <p class="text-body-md-500 text-dark text-center">
+                У журналі "дев'ять" ви знайдете цікаві статті про новітні
+                тенденції у світі мистецтва та літератури та роздуми про
+                глибинні філософські питання.
+              </p>
+            </div>
+            <div class="flex gap-4">
+              <NuxtLink :to="localePath('/', $i18n.locale)">
+                <Button
+                  name="на головну"
+                  class="border text-dark border-dark"
+                />
+              </NuxtLink>
+              <!-- <NuxtLink :to="localePath('/journal/view', $i18n.locale)"> -->
+              <Button
+                name="дивитись"
+                class="bg-dark text-light"
+                @click="store.toggleGatherPages"
+              />
+              <!-- </NuxtLink> -->
+            </div>
           </div>
         </div>
       </div>
@@ -87,6 +100,7 @@
 .swiper-slide-active {
   @apply z-20;
 }
+
 .fade-enter-from {
   @apply opacity-0;
 }
