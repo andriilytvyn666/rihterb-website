38b297c6adc6e505b660a30dd389549e17cfc86c
Andrii <lytvyn349@gmail.com>
2023-04-27 16:24:40 +0300

    feat: use different image components on mobile and desktop in minecraft section

--- nuxt/components/Minecraft/index.vue
+++ nuxt/components/Minecraft/index.vue
@@ -2,31 +2,37 @@
   <div
     class="flex flex-col items-center justify-between gap-8 p-8 overflow-hidden rounded-lg bg-dark-alt"
   >
-    <Swiper
-      id="swiper"
-      free-mode
-      :slides-per-view="1"
-      :space-between="32"
-      :breakpoints="{
-        768: {
-          slidesPerView: 2,
-        },
-      }"
-      class="h-[20.5rem]"
-    >
-      <SwiperSlide
-        v-for="image in minecraft.images"
-        :key="image.asset._ref"
-        class="page"
+    <div class="hidden sm:block">
+      <Swiper
+        id="swiper"
+        free-mode
+        :slides-per-view="1"
+        :space-between="32"
+        :breakpoints="{
+          768: {
+            slidesPerView: 2,
+          },
+        }"
+        class="h-[20.5rem]"
       >
-        <SanityImage
-          rel="preload"
-          height="327"
-          :asset-id="image.asset._ref"
-          class="image"
-        />
-      </SwiperSlide>
-    </Swiper>
+        <SwiperSlide
+          v-for="image in minecraft.images"
+          :key="image.asset._ref"
+          class="page"
+        >
+          <SanityImage
+            rel="preload"
+            height="327"
+            :asset-id="image.asset._ref"
+            class="image"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+    <SanityImage
+      :asset-id="minecraftPage.image.asset._ref"
+      class="object-cover rounded-lg aspect-square shadow-default sm:hidden"
+    />
     <div
       class="flex flex-col justify-between w-full gap-8 sm:gap-0 sm:flex-row"
     >
@@ -52,6 +58,7 @@
 <script lang="ts" setup>
 const store = useSanityStore()
 const minecraft = (await store.getMainPage()).minecraft
+const minecraftPage = await store.getMinecraftPage()
 const localePath = useLocalePath()
 </script>
 
