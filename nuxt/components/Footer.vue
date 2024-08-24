6b8cd7bb68092fbe3c2e4a55c0ccdc026e4055e8
Andrii Lytvyn <lytvyn349@gmail.com>
2024-08-24 20:48:32 +0300

    fix footer font

--- nuxt/components/Footer.vue
+++ nuxt/components/Footer.vue
@@ -5,27 +5,27 @@
     <div class="flex flex-col w-full gap-2 md:gap-0 font-inter text-bg1">
       <div class="flex gap-2">
         <button
-          :class="`${$i18n.locale === 'en' ? 'text-fg2' : 'hover:text-fg2'} text-footer`"
+          :class="`${$i18n.locale === 'en' ? 'text-fg2' : 'hover:text-fg2'} text-footer font-inter`"
           @click="setLocale('en')"
         >
           English
         </button>
         <button
-          :class="` ${['ru', 'bel', 'uk'].includes($i18n.locale) ? 'text-fg2' : 'hover:text-fg2'} text-footer`"
+          :class="` ${['ru', 'bel', 'uk'].includes($i18n.locale) ? 'text-fg2' : 'hover:text-fg2'} text-footer font-inter`"
           @click="setLocale('uk')"
         >
           Українська
         </button>
       </div>
       <div class="flex flex-col gap-0 md:flex-row md:gap-2">
-        <span class="text-footer line-clamp-1"
+        <span class="font-inter text-footer line-clamp-1"
           >Copyright © 2023 Василій Ріхтер</span
         >
         <span class="hidden md:inline">/</span>
         <NuxtLink
           to="https://nneeeooo.pp.ua"
           target="_blank"
-          class="hover:text-fg2 hover:underline text-footer line-clamp-1"
+          class="font-inter hover:text-fg2 hover:underline text-footer line-clamp-1"
         >
           Design & Development by Andrii Lytvyn
         </NuxtLink>
