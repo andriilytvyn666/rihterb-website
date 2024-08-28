4a85bc761bfc54a3cd4057ed27652d5f30d95309
Andrii Lytvyn <lytvyn349@gmail.com>
2024-08-28 15:37:08 +0000

    fix about buttons and grid again

--- nuxt/components/About.vue
+++ nuxt/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col gap-4 sm:max-w-[28rem] lg:w-full xl:max-w-[25rem]">
-    <div class="flex border rounded-2xl aspect-square overflow-clip border-bg1">
+    <div class="flex overflow-clip rounded-2xl border aspect-square border-bg1">
       <NuxtImg
         provider="sanity"
         sizes="600px sm:800px lg:600px"
@@ -13,17 +13,17 @@
     <div class="flex flex-col gap-3">
       <div class="flex flex-col gap-1">
         <h1
-          class="font-inter text-title text-accent leading-1 whitespace-nowrap"
+          class="whitespace-nowrap font-inter text-title text-accent leading-1"
         >
           {{ getLocalizedString($i18n.locale, name) }}
         </h1>
-        <h2 class="font-sans leading- whitespace-nowrap text-body">
+        <h2 class="font-sans whitespace-nowrap leading- text-body">
           {{ getLocalizedString($i18n.locale, subtitle) }}
         </h2>
       </div>
-      <div class="flex w-full gap-2 flex-nowrap">
+      <div class="flex flex-nowrap gap-2 w-full">
         <ButtonLink
-          class="w-full lg:w-fit"
+          class="w-full md:w-fit"
           v-for="button in buttons"
           :key="button.title.en"
           :title="getLocalizedString($i18n.locale, button.title)"
